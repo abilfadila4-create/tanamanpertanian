524af864dcde9785a9de4e333afa23cda6db89bb
abilfadila4-create <abilfadila4@gmail.com>
2025-12-13 17:30:27 +0700

    pertanian_presisi.py

--- pertanian_presisi.py
+++ pertanian_presisi.py
@@ -11,7 +11,7 @@ import io
 
 # --- 1. Konfigurasi Streamlit ---
 st.set_page_config(
-    page_title="Websitte Rekomendasi Tanaman Pertanian Presisi",
+    page_title="Aplikasi Rekomendasi Tanaman Pertanian Presisi",
     layout="wide",
     initial_sidebar_state="expanded"
 )
@@ -246,3 +246,4 @@ with tab4:
     *Dibuat dengan Python, Streamlit, dan Scikit-learn.*
 
     """)
+
